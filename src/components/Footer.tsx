e729a8294a7dd1c25a724eb7f4891d18a5c1dd63
sth3r <vrodrigues.esther@Gmail.com>
2025-12-05 04:56:46 -0300

    nao to entendendo

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+  <footer id="contato" className="footer">
+    <p>Contato:
+      <a href="mailto:vrodrigues.esther@gmail.com">vrodrigues.esther@gmail.com</a> |
+      <a href="https://www.linkedin.com/in/esther-rodrigues/" target="_blank">LinkedIn</a> |
+      <a href="https://github.com/sth3r" target="_blank">GitHub</a> |
+      <a href="https://www.behance.net/esthervrodrigues" target="_blank">Behance</a>
+    </p>
+    <p>&copy; 2025 Esther Rodrigues — Todos os direitos reservados.</p>
+  </footer>
+  );
+}
