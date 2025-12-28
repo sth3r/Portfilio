0f4742417274f86352b2ff5f9ad9b465da9563ae
sth3r <vrodrigues.esther@Gmail.com>
2025-12-28 00:23:20 -0300

    vai?

--- src/pages/services/[slug].tsx
+++ src/pages/services/[slug].tsx
@@ -1,21 +1,21 @@
-import { useParams } from "react-router-dom";
-import { services } from "../../content/services.data";
+// import { useParams } from "react-router-dom";
+// import { services } from "../../content/services.data";
 
-export default function ServicePage({ lang }: any) {
-  const { slug } = useParams();
-  const service = services.find((s) => s.slug === slug);
+// export default function ServicePage({ lang }: any) {
+//   const { slug } = useParams();
+//   const service = services.find((s) => s.slug === slug);
 
-  if (!service) return null;
+//   if (!service) return null;
 
-  return (
-    <section className="service-page">
-      <h1>{service.title[lang]}</h1>
+//   return (
+//     <section className="service-page">
+//       <h1>{service.title[lang]}</h1>
 
-      <ul>
-        {service.items[lang].map((item: string) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </section>
-  );
-}
+//       <ul>
+//         {service.items[lang].map((item: string) => (
+//           <li key={item}>{item}</li>
+//         ))}
+//       </ul>
+//     </section>
+//   );
+// }
